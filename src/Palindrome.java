09bb69148865ed76576496a8fbebd907ad293e6e
Cahyo <Cahyo>
2016-12-22 17:04:46 +0700

    Initial commit

---
+++ src/Palindrome.java
@@ -0,0 +1,29 @@
+/**
+ * Created by user on 22/12/2016.
+ */
+public class Palindrome {
+    /* Palindrome Program In JAVA
+  © Code Nirvana (www.codenirvana.in)
+*/
+import java.util.Scanner;
+    class Palindrome{
+        public static void main(String args[]){
+            System.out.print("Enter Number: ");
+            Scanner read = new Scanner(System.in);
+            int num = read.nextInt();
+            int n = num;
+            //reversing number
+            int rev=0,rmd;
+            while(num > 0)
+            {
+                rmd = num % 10;
+                rev = rev * 10 + rmd;
+                num = num / 10;
+            }
+            if(rev == n)
+                System.out.println(n+" is a Palindrome Number!");
+            else
+                System.out.println(n+" is not a Palindrome Number!");
+        }
+    }
+}
